1afa9d27e189a6a01a6c8919be0f475a41350ae2
Entoni1122 <entonishkalla34@gmail.com>
2024-05-24 10:27:25 +0200

    Camera Adjustment

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 OffSet;
+    [SerializeField] float lerpSpeed;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + OffSet, lerpSpeed);
+    }
+}
